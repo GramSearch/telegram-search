1b5778421aef380653c3fe84338996227e9a9e46
RainbowBird <git@luoling.moe>
2025-06-28 17:41:53 +0800

    fix: build

--- packages/core/src/context.ts
+++ packages/core/src/context.ts
@@ -7,7 +7,6 @@ import type { ConnectionEventFromCore, ConnectionEventToCore } from './services/
 import type { DialogEventFromCore, DialogEventToCore } from './services/dialog'
 import type { EntityEventFromCore, EntityEventToCore } from './services/entity'
 import type { GramEventsEventFromCore, GramEventsEventToCore } from './services/gram-events'
-import type { MediaEventFromCore, MediaEventToCore } from './services/media'
 import type { MessageEventFromCore, MessageEventToCore } from './services/message'
 import type { StorageEventFromCore, StorageEventToCore } from './services/storage'
 import type { TakeoutEventFromCore, TakeoutEventToCore } from './services/takeout'
@@ -27,7 +26,6 @@ export type FromCoreEvent = ClientInstanceEventFromCore
   & StorageEventFromCore
   & ConfigEventFromCore
   & GramEventsEventFromCore
-  & MediaEventFromCore
 
 export type ToCoreEvent = ClientInstanceEventToCore
   & MessageEventToCore
@@ -39,7 +37,6 @@ export type ToCoreEvent = ClientInstanceEventToCore
   & StorageEventToCore
   & ConfigEventToCore
   & GramEventsEventToCore
-  & MediaEventToCore
 
 export type CoreEvent = FromCoreEvent & ToCoreEvent
 
